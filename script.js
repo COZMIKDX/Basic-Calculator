455294befbb7e476301a2f916c6b3f8e70f1893b
COZMIKDX <tacocatdx@gmail.com>
2023-10-04 17:33:37 -0500

    Helper sliders

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let element = document.getElementById("testelem");
+let slider = document.getElementById("slider");
+let label = document.getElementById("sliderlabel");
+slider.addEventListener("input", () => {
+    element.style.width = `${slider.value}px`;
+    label.innerHTML = `${slider.value}%`;
+});
+
+let slider2 = document.getElementById("slider2");
+let label2 = document.getElementById("sliderlabel2");
+slider2.addEventListener("input", () => {
+    element.style.height = `${slider2.value}px`;
+    label2.innerHTML = `${slider2.value}%`;
+});
+*/
